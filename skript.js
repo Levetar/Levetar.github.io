113408761468123dca56da87104a0264476ba284
Il'ya <popov.il95@mail.ru>
2022-01-14 14:53:15 +0300

    Done
    Добавил другой способ исчезновения первой строчки
    Т.е. теперь обе строчки исчезают по-разному (появляются при повторном нажатии) и удаляются, при нажатии на пробел.

--- skript.js
+++ skript.js
@@ -1,6 +1,11 @@
-$('#name, #post').on('click', function () {
+$('#boxName').on('click', function () {
+	$('#name').toggleClass('_remove');
+});
+
+$('#post').on('click', function () {
 	$(this).toggleClass('_active');
-})
+});
+
 
 $(window).keypress(function(e) {
     if (e.which === 32) {
